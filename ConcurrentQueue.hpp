33c198c4ea9b969d0c6c58b52518a17abdc23bab
Benjamin <bavogel@iastate.edu>
2019-11-22 18:24:55 -0600

    Added concurrency queue for threads

---
+++ ConcurrentQueue.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <pthread.h>
+#include <queue>
+#include <string>
+
+class ConcurrentQueue
+{
+    private:
+        std::queue<std::string> q;
+        pthread_mutex_t mutex;
+
+    public:
+        ConcurrentQueue()
+        {
+            q = std::queue<std::string>();
+            pthread_mutex_init(&mutex, NULL);
+        }
+
+        ~ConcurrentQueue()
+        {
+            pthread_mutex_destroy(&mutex);
+        }
+
+        void push(std::string message)
+        {
+            pthread_mutex_lock(&mutex);
+            q.push(message);
+            pthread_mutex_unlock(&mutex);
+        }
+
+        std::string pop()
+        {
+            pthread_mutex_lock(&mutex);
+            std::string message = q.front();
+            q.pop();
+            pthread_mutex_unlock(&mutex);
+            return message;
+        }
+
+};
--- RBTreeRun.cpp
+++ RBTreeRun.cpp
@@ -5,55 +5,83 @@
 
 #include <fstream>
 #include <iostream>
+#include <pthread.h>
+
+void threadRun(std::vector<Command> commands, RBTree& rbtree)
+{
+    for (auto i = commands.begin(); i != commands.end(); ++i)
+    {
+        Command command = (*i);
+        switch (command.getCommand())
+        {
+        case CommandType::SEARCH:
+            rbtree.search(command.getNode());
+            break;
+        case CommandType::INSERT:
+            rbtree.insert(command.getNode());
+            break;
+        case CommandType::DELETE:
+            rbtree.deleteNode(command.getNode());
+            break;
+        default:
+            std::cout << "Not a valid command" << std::endl;
+            break;
+        }
+    }
+
+    pthread_exit(NULL);
+}
 
 int main(int argc, char *argv[])
 {
-    RBTree tree = RBTree();
+    // RBTree tree = RBTree();
 
-    Node *node = new Node(7, Color::BLACK);
-    tree.insertNode(node);
+    // Node *node = new Node(7, Color::BLACK);
+    // tree.insertNode(node);
 
-    node = new Node(3, Color::BLACK);
-    tree.insertNode(node);
+    // node = new Node(3, Color::BLACK);
+    // tree.insertNode(node);
 
-    node = new Node(18, Color::RED);
-    tree.insertNode(node);
+    // node = new Node(18, Color::RED);
+    // tree.insertNode(node);
 
-    node = new Node(10, Color::BLACK);
-    tree.insertNode(node);
+    // node = new Node(10, Color::BLACK);
+    // tree.insertNode(node);
 
-    node = new Node(8, Color::RED);
-    tree.insertNode(node);
+    // node = new Node(8, Color::RED);
+    // tree.insertNode(node);
 
-    node = new Node(11, Color::RED);
-    tree.insertNode(node);
+    // node = new Node(11, Color::RED);
+    // tree.insertNode(node);
 
-    node = new Node(22, Color::BLACK);
-    tree.insertNode(node);
+    // node = new Node(22, Color::BLACK);
+    // tree.insertNode(node);
 
-    node = new Node(26, Color::RED);
-    tree.insertNode(node);
+    // node = new Node(26, Color::RED);
+    // tree.insertNode(node);
 
-    tree.print();
+    // tree.print();
 
-    bool found = tree.search(7);
-    std::cout << "Finding node 7: " << found << std::endl;
+    // bool found = tree.search(7);
+    // std::cout << "Finding node 7: " << found << std::endl;
 
-    tree.deleteNode(7);
+    // tree.deleteNode(7);
 
-    found = tree.search(7);
-    std::cout << "Finding node 7: " << found << std::endl;
+    // found = tree.search(7);
+    // std::cout << "Finding node 7: " << found << std::endl;
 
-    tree.print();
+    // tree.print();
 
     Parser parser = Parser();
     FileOutput output = parser.parse(argv[1]);
+    RBTree fileTree = RBTree();
 
     std::vector<Node *> nodes = output.getNodes();
 
     for (auto i = nodes.begin(); i != nodes.end(); ++i)
     {
         std::cout << "Node: " << (*i)->getKey() << std::endl;
+        fileTree.insertNode(*i);
     }
 
     std::vector<std::vector<Command>> commands = output.getCommands();
